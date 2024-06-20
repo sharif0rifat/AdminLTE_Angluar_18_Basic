7746b70947935294973df6f75616390763218da8
sharif0rifat <sharif0rifat@gmail.com>
2024-06-20 18:56:54 +0600

    New component added for header and side bar

--- AdminDemo/src/app/app.component.ts
+++ AdminDemo/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from './core/side-bar/side-bar.component';
+import { HeaderComponent } from './core/header/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SideBarComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
